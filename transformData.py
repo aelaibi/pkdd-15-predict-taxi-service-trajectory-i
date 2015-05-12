c9be01f3f4318ee989f191a21390ae43bbdd0912
Abdelhaq El Aibi <aelaibi@octo.com>
2015-05-12 17:04:54 +0100

    new transformer : adding more cols (hour, month, week,)

--- transformData.py
+++ transformData.py
@@ -14,6 +14,7 @@ test = 'data/test.csv'
 #"TRIP_ID","CALL_TYPE","ORIGIN_CALL","ORIGIN_STAND","TAXI_ID","TIMESTAMP","DAY_TYPE","MISSING_DATA","POLYLINE"
 header = "TRIP_ID,CALL_TYPE,ORIGIN_CALL,ORIGIN_STAND,TAXI_ID,TIMESTAMP,DAY_TYPE,MISSING_DATA,LAT1,LONG1,LAT2,LONG2"
 yHead = ",LATF,LONGF"
+extraCols =",MONTH,WEEK,DAY,HOUR"
 
 def data(path, traindata=False):
     for t, line in enumerate(open(path)):
@@ -21,6 +22,7 @@ def data(path, traindata=False):
             continue
         # parse x
         line2 = ""
+        d = 1
         for m, feat in enumerate(line.rstrip().replace('NA', '""').replace('","',';').replace('"','').split(';')):
             if m == 8: # POLYLINE
                 polyline = json.loads(feat)
@@ -41,7 +43,8 @@ def data(path, traindata=False):
                     long2 = polyline[-2][1]
                 except:
                     long2 = -999
-
+                line2+=",%s,%s,%s,%s" % (lat1, long1, lat2, long2)
+                line2+=",%s,%s,%s,%s" % (d.month, d.isocalendar()[1], d.day, d.hour)
                 if traindata:
                     try:
                         long_final = polyline[-1][1]
@@ -52,11 +55,14 @@ def data(path, traindata=False):
                     except:
                         lat_final = -999
                     #append data
-                    line2+=",%s,%s,%s,%s,%s,%s" % (lat1, long1, lat2, long2, lat_final, long_final)
-                else:
-                    line2+=",%s,%s,%s,%s" % (lat1, long1, lat2, long2)
-            else:
+                    line2+=",%s,%s" % (lat_final, long_final)
+
+            elif m == 5:
+                d = datetime.fromtimestamp(int(feat))
                 line2+=feat if m == 0 else ","+feat
+            else :
+                line2+=feat if m == 0 else ","+feat
+
 
         yield (line2)
 
@@ -65,7 +71,7 @@ tt = 1
 
 print 'transforming train data'
 with open("data/train2.csv", 'w') as outfile:
-    outfile.write(header+yHead+'\n')
+    outfile.write(header+extraCols+yHead+'\n')
     for line in data(train, traindata = True):
         outfile.write('%s\n' % line)
         if tt % 100000 == 0:
@@ -76,7 +82,7 @@ with open("data/train2.csv", 'w') as outfile:
 tt = 1
 print 'transforming test data'
 with open("data/test2.csv", 'w') as outfile:
-    outfile.write(header+'\n')
+    outfile.write(header+extraCols+'\n')
     for lineTest in data(test):
         outfile.write('%s\n' % lineTest)
         if tt % 100000 == 0:
